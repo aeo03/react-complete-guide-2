57936ab6f1eeacd4ad405b85a48001fd658301fd
aeo03 <franzedwarddayrit03@yahoo.com>
2020-10-28 20:54:14 +0800

    franz-dev: Modified name and age to be rendered thru state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    person: [
+      {name: "Max", age: 28},
+      {name: "Manu", age: 27},
+      {name: "Stephanie", age: 26}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working now!</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Biking</Person>
-        <Person name="Stephanie" age="26"/>
+        <button>Switch Name</button>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Biking</Person>
+        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
       </div>
     );
   }
