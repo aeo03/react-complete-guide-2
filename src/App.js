a074d05710c99c61449151d259e0819764aaea47
aeo03 <franzedwarddayrit03@yahoo.com>
2020-10-29 21:54:45 +0800

    Used bind to pass a variable to other component

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was Clicked')
     this.setState({
       person: [
-        {name: "Maximillian", age: 28},
+        {name: newName, age: 28},
         {name: "Manu", age: 27},
         {name: "Stephanie", age: 27}
       ]
@@ -27,10 +27,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working now!</p>
-        <button onClick={this.switchNameHandler} >Switch Name</button>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-        <Person name={this.state.person[1].name} age={this.state.person[1].age}>My Hobbies: Biking</Person>
-        <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')} >Switch Name</button>
+        <Person 
+          name={this.state.person[0].name} 
+          age={this.state.person[0].age}/>
+        <Person 
+          name={this.state.person[1].name} 
+          age={this.state.person[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!!!')}>My Hobbies: Biking</Person>
+        <Person 
+          name={this.state.person[2].name} 
+          age={this.state.person[2].age}/>
       </div>
     );
   }
