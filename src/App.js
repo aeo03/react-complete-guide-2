f6832be493d35d95537176ab7cb09e8ae2f0d106
aeo03 <franzedwarddayrit03@yahoo.com>
2020-10-30 08:36:06 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -33,11 +33,21 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      padding: '8px',
+      cursor: 'pointer',
+      border: '1px solid blue'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is working now!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')} >Switch Name</button>
+        <button
+          style={style} 
+          onClick={this.switchNameHandler.bind(this, 'Maximilian')} >Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/>
