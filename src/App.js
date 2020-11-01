1ac0b4268e1e9011224478afb53dbe431d43082f
Franz <Franz@LAPTOP-JC7PJSNR>
2020-11-01 12:34:29 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -108,9 +108,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is working now!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <button className="button" onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         { persons }
       </div>
     );
